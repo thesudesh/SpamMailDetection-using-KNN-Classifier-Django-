e17f257adee5f1d04d50d39f2614a3ee0279c7f9
theSudesh <xudes00@gmail.com>
2024-03-10 15:09:05 +0545

    completed single message detection

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from core.models import Comment
+# from core.models import Comment
 from core.spam_model import predict_spam 
 from django.http import HttpResponse
 
@@ -90,29 +90,36 @@ def signin(request):
         if user is not None:
             login(request, user)
             fname = user.first_name
-            return render(request, "comments.html", {'fname': fname})
+            return redirect('home')
         else:
             messages.error(request, "Invalid Credentials")
             return redirect('home')
 
     return render(request, "signin.html")
 
-def comments(request):
-    comments = Comment.objects.all()
-    context = {'comments':comments}
-    return render(request, 'comments.html',context)
+# def comments(request):
+#     comments = Comment.objects.all()
+#     context = {'comments':comments}
+#     return render(request, 'comments.html',context)
 
-def check_spam(request):
-    comments = Comment.objects.all()
-    predictions = predict_spam(comments.values_list('text', flat=True))
-    print(predictions)
-    context = {'comments':zip(comments, predictions)}
-    return render(request, 'partials/comments-spam.html',context)
+# def check_spam(request):
+#     comments = Comment.objects.all()
+#     predictions = predict_spam(comments.values_list('text', flat=True))
+#     print(predictions)
+#     context = {'comments':zip(comments, predictions)}
+#     return render(request, 'partials/comments-spam.html',context)
 
-def delete_comment(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    comment.delete()
-    return HttpResponse(status=204)
+def check(request):
+    if request.method == "POST":
+        comment = request.POST.get("comment")
+        res = predict_spam(comment)[0]
+        res = 'Spam' if res else 'Not Spam'
+    return render(request, 'index.html', {'res':res})
+
+# def delete_comment(request, pk):
+#     comment = get_object_or_404(Comment, pk=pk)
+#     comment.delete()
+#     return HttpResponse(status=204)
 
 
 def signout(request):
